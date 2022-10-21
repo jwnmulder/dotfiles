0f1da8c7ac1218d3c1fd5f9cdc62479121c7729d
Jan-Willem Mulder <jwnmulder@gmail.com>
2022-10-21 23:16:03 +0200

    updated batcat alias

--- .config/zsh/commands.zsh
+++ .config/zsh/commands.zsh
@@ -1,6 +1,6 @@
 if (( $+commands[batcat] )); then
-    alias cat='batcat --paging=never'
+    alias cat='batcat --paging=never --plain'
     alias bat='batcat'
 elif (( $+commands[bat] )); then
-    alias cat='bat --paging=never'
+    alias cat='bat --paging=never --plain'
 fi
