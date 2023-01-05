1936fcb0587358a02791c134183a9261e3e5af4d
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-01-05 13:58:19 +0100

    refactored initial age key decryption

---
+++ .chezmoiscripts/decrypt-private-key-instructions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ENCRYPTED_KEY="$1/key.txt.age"
+AGE_BIN=age
+AGE_DOWNLOAD_URL="https://github.com/FiloSottile/age/releases/download/v1.1.1/age-v1.1.1-linux-amd64.tar.gz"
+
+if ! command -v "$AGE_BIN" &> /dev/null; then
+    AGE_TMP_DIR="/tmp/age"
+    rm -rf "$AGE_TMP_DIR"; mkdir -p "$AGE_TMP_DIR"
+    curl -sL "$AGE_DOWNLOAD_URL" | tar -xz --strip-components 1 -C "$AGE_TMP_DIR"
+    AGE_BIN="$AGE_TMP_DIR/age"
+    chmod +x "$AGE_BIN"
+fi
+
+echo Decrypt the age key by running: "$AGE_BIN" --decrypt --output "${HOME}/.config/chezmoi/key.txt" "$ENCRYPTED_KEY"
