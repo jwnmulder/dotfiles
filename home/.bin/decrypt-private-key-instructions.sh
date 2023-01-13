c8764f1161d79655792062167d7876f75b9b5ff7
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-01-13 23:17:50 +0100

    arm support

--- home/.bin/decrypt-private-key-instructions.sh
+++ home/.bin/decrypt-private-key-instructions.sh
@@ -2,9 +2,17 @@
 
 set -euo pipefail
 
+architecture=""
+case $(uname -m) in
+    i386 | i686) architecture="386" ;;
+    x86_64)      architecture="amd64" ;;
+    arm)         dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+    *)           echo "Unable to determine system architecture."; exit 1 ;;
+esac
+
 ENCRYPTED_KEY="$1/.data/key.txt.age"
 AGE_BIN=age
-AGE_DOWNLOAD_URL="https://github.com/FiloSottile/age/releases/download/v1.1.1/age-v1.1.1-linux-amd64.tar.gz"
+AGE_DOWNLOAD_URL="https://github.com/FiloSottile/age/releases/download/v1.1.1/age-v1.1.1-linux-${architecture}.tar.gz"
 
 if ! command -v "$AGE_BIN" &> /dev/null; then
     AGE_TMP_DIR="/tmp/age"
