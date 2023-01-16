f31fd28b0d59ce980105d33ea6621e4807e1553b
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-01-16 23:21:49 +0100

    armv5tel improvements

--- home/.bin/decrypt-private-key-instructions.sh
+++ home/.bin/decrypt-private-key-instructions.sh
@@ -7,7 +7,7 @@ case $(uname -m) in
     i386 | i686) architecture="386" ;;
     x86_64)      architecture="amd64" ;;
     arm)         dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
-    *)           echo "Unable to determine system architecture."; exit 1 ;;
+    *)           echo "No age crypto support for $(uname -m)"; exit 0 ;;
 esac
 
 ENCRYPTED_KEY="$1/.data/key.txt.age"
