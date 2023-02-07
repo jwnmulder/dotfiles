0d85def5442d42cb8a117d74ea82c17ae1927a51
Jan-Willem Mulder <jan-willem.mulder@iodigital.com>
2023-02-07 22:31:47 +0100

    Windows file restructuring

--- home/.chezmoitemplates/powershell-profile.ps1
+++ home/.chezmoitemplates/powershell-profile.ps1
@@ -1,6 +1,3 @@
-# Set-Alias ngen (Join-Path ([Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()) ngen.exe)
-# ngen update
-
 $env:STARSHIP_CONFIG = "$HOME\AppData\Local\starship\starship.toml"
 Invoke-Expression (&"$HOME\AppData\Local\starship\starship.exe" init powershell)
    
