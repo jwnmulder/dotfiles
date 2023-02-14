bf094fba4048358207840e1aa29121f19c060b92
Jan-Willem Mulder <jan-willem.mulder@iodigital.com>
2023-02-14 13:39:52 +0100

    Workaround for Windows OneDrive personal folders

--- home/.chezmoiscripts/run_after_sync_powershell_profiles.ps1
+++ home/.chezmoiscripts/run_after_sync_powershell_profiles.ps1
@@ -1,5 +1,6 @@
+$userprofile_documents = "$env:USERPROFILE\Documents"
 $mydocuments = [environment]::getfolderpath("mydocuments")
 
-if (-Not $mydocuments.ToLower().StartsWith($env:USERPROFILE.ToLower())) {
-    Copy-Item "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Destination "$mydocuments\WindowsPowerShell\" -Recurse
+if (-Not $mydocuments.ToLower().StartsWith($userprofile_documents.ToLower())) {
+    Copy-Item "$userprofile_documents\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Destination "$mydocuments\WindowsPowerShell\" -Recurse
 } 
