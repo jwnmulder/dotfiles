dfdd3d466b9208db06f0ff62f3d6b5bb4df58f55
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-04-13 10:16:12 +0200

    Temp fix for font update issues on Windows

--- home/.chezmoiscripts/run_after_31_register_user_fonts.ps1
+++ home/.chezmoiscripts/run_after_31_register_user_fonts.ps1
@@ -1,6 +1,13 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
+# TODO: Updating Fonts in place was causing issues. Chezmoi will now download fonts to .cache/chezmoi/fonts"
+# which need to be installed by this script. For that we need to:
+# 1. check if a font is new or updated
+# 2. if updated, de-register font from regedit (and hope that Windows releases the file handle)
+# 3. Copy the font file from .cache/chezmoi/fonts to AppData/Local/Microsoft/Windows/Fonts
+# 4. re-register the font (this is the script below)
+
 $shell = New-Object -ComObject Shell.Application
 # $FONTS = 0x14
 # $objFolder = $shell.Namespace($FONTS)
