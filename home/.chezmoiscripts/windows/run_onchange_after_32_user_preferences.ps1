49be4cb6bc34a6bddf48d657099bb66421f7dcd0
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-04-15 15:14:53 +0200

    some small refactoring

---
+++ home/.chezmoiscripts/windows/run_onchange_after_32_user_preferences.ps1
@@ -0,0 +1,5 @@
+# ------------------------------------------------------------------------------
+#                User Preferences: Explorer, Taskbar, and System Tray
+# ------------------------------------------------------------------------------
+
+Write-Output "Configuring Explorer, Taskbar, and System Tray..."
