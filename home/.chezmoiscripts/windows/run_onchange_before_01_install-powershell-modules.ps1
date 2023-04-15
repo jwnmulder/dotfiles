6c97f123ea2030087bfb73acca590b1144f9fb37
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-04-16 00:12:55 +0200

    cleanup

--- home/.chezmoiscripts/windows/run_onchange_before_01_install-powershell-modules.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_01_install-powershell-modules.ps1
@@ -1,5 +1,10 @@
-Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
+if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+    Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
+}
 
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+if (-not (Get-PSRepository -Name "PSGallery").InstallationPolicy -eq "Trusted") {
+    Write-Output "PSGallery InstallationPolicy Trusted"
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+}
 
 Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
