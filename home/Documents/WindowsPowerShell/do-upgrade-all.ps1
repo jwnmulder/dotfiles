1f121c990a1b13f09a9c1a2e2d18513c222be1ae
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-06-30 22:01:59 +0200

    Added scoop update to do-upgrade-all

--- home/Documents/WindowsPowerShell/do-upgrade-all.ps1
+++ home/Documents/WindowsPowerShell/do-upgrade-all.ps1
@@ -1,17 +1,36 @@
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-$IsAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
 if (Get-Command winget -ErrorAction SilentlyContinue) {
 
-    $wingetUpgradeCommand = "winget upgrade --all --silent"
+    $cmd = "winget upgrade --all --silent"
 
-    if (-not $IsAdmin) {
-        $wingetUpgradeCommand += " --scope=user"
+    if (-not $isAdmin) {
+        $cmd += " --scope=user"
     }
 
-    Write-Host "# winget - Running $wingetUpgradeCommand"
-    Invoke-Expression $wingetUpgradeCommand
-    Write-Host "# winget - Done running $wingetUpgradeCommand"
+    Write-Host "# winget - Running $cmd"
+    Invoke-Expression $cmd
+    Write-Host "# winget - Done running $cmd" -ForegroundColor Green
+}
+
+if (Get-Command scoop -ErrorAction SilentlyContinue) {
+
+    $cmd = "scoop update --all"
+
+    if ($isAdmin) {
+
+        Write-Host "# scoop - Skipping $cmd as we are running as Administrator" -ForegroundColor Cyan
+
+    } else {
+
+        Write-Host "# scoop - Ensure correct permissions $cmd"
+        takeown /f $env:USERPROFILE\scoop\buckets /r >$null
+
+        Write-Host "# scoop - Running $cmd"
+        Invoke-Expression $cmd
+        Write-Host "# scoop - Done running $cmd" -ForegroundColor Green
+    }
 }
 
 Write-Host "do-upgrade-all - All done"
