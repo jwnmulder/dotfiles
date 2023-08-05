d87da22f78323cfd27a8ed1a29e55c4c7be74308
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-08-05 20:23:27 +0200

    windows user env configuration script

---
+++ home/.chezmoiscripts/windows/run_onchange_before_02_environment.ps1
@@ -0,0 +1,34 @@
+Set-StrictMode -Version latest
+$ErrorActionPreference = "Stop"
+
+Add-EnvPath -Container User -Path "$env:USERPROFILE\.local\bin"
+
+function Add-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session',
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
+        if ($persistedPaths -notcontains $Path) {
+            $persistedPaths = $persistedPaths + $Path | Where-Object { $_ }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -notcontains $Path) {
+        $envPaths = $envPaths + $Path | Where-Object { $_ }
+        $env:Path = $envPaths -join ';'
+    }
+}
