dd25954694ad6a9043f2c568d9a7bcb1f0798853
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-08-07 17:39:33 +0200

    added scoop extras bucket

--- home/.chezmoiscripts/windows/run_onchange_before_03_install-scoop.ps1
+++
@@ -1,8 +0,0 @@
-Set-StrictMode -Version 3.0
-$ErrorActionPreference = "Stop"
-
-if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing scoop"
-    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-    Invoke-RestMethod get.scoop.sh | Invoke-Expression
-}
