3df91cb7793e7463b85a9650e44ab4ade35b79ed
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-08-09 18:24:30 +0200

    Predictive IntelliSense for powershell

--- home/.chezmoiscripts/windows/run_onchange_before_01_install-powershell-modules.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_01_install-powershell-modules.ps1
@@ -1,7 +1,7 @@
 Set-StrictMode -Version 3.0
 $ErrorActionPreference = "Stop"
 
-# Needed for GitHubs 'windows-latest' image. SOmehow version 1.4.7 is selected which is not working
+# Needed for GitHubs 'windows-latest' image. Somehow version 1.4.7 is selected which is not working
 Import-Module PackageManagement -MinimumVersion 1.4.8
 
 # '-ForceBootstrap' will Install the NuGet package provider if not already done
@@ -11,10 +11,17 @@ if (-not ((Get-PSRepository -Name "PSGallery" -ErrorAction SilentlyContinue).Ins
     Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 }
 
+if (-not (Get-InstalledModule -Name "PowerShellGet" -MinimumVersion 2.0 -ErrorAction SilentlyContinue)) {
+    Install-Module PowerShellGet -Scope CurrentUser -MinimumVersion 2.0 -Force -AllowClobber
+}
+
+# PSScriptAnalyzer
 if (-not (Get-Module -Name PSScriptAnalyzer -ListAvailable)) {
     Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
 }
 
-if (-not (Get-InstalledModule -Name "PowerShellGet" -MinimumVersion 2.0 -ErrorAction SilentlyContinue)) {
-    Install-Module PowerShellGet -Scope CurrentUser -MinimumVersion 2.0 -Force -AllowClobber
+# 2.1.0 has support for Predictive IntelliSense (F2 switches between InlineView and ListView)
+# https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/using-predictors?view=powershell-7.3$
+if (-not (Get-InstalledModule -Name "PSReadLine" -MinimumVersion 2.2.6 -ErrorAction SilentlyContinue)) {
+    Install-Module PSReadLine -Scope CurrentUser -MinimumVersion 2.2.6 -Force -AllowClobber
 }
