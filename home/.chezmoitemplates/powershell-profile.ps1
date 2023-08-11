5e8dfe3d4d9efcdff0615bfc81604da23b7608cc
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-08-11 21:16:36 +0200

    chezmoi completions for powershell

--- home/.chezmoitemplates/powershell-profile.ps1
+++ home/.chezmoitemplates/powershell-profile.ps1
@@ -2,3 +2,5 @@ $env:STARSHIP_CONFIG = "$HOME\AppData\Local\starship\starship.toml"
 Invoke-Expression (&"$HOME\AppData\Local\starship\starship.exe" init powershell)
 
 Set-Alias do-upgrade-all $HOME\Documents\WindowsPowerShell\do-upgrade-all.ps1
+
+. $HOME\Documents\WindowsPowerShell\completions\_chezmoi.ps1
