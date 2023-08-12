4584bd804e9ad6e51876deebc1cf5243f671fae2
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-08-12 12:03:01 +0200

    windows env setup bugfixes

--- home/.chezmoiscripts/windows/run_onchange_before_02_environment.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_02_environment.ps1
@@ -10,24 +10,33 @@ function Add-EnvPath {
         [string] $Container = 'Session'
     )
 
+    <#
+    Using the .NET registry calls is necessary here in order to preserve environment variables embedded in PATH values;
+    Powershell's registry provider doesn't provide a method of preserving variable references, and we don't want to
+    accidentally overwrite them with absolute path values. Where the registry allows us to see "%SystemRoot%" in a PATH
+    entry, PowerShell's registry provider only sees "C:\Windows", for example.
+    #>
     if ($Container -ne 'Session') {
-        $containerMapping = @{
-            Machine = [EnvironmentVariableTarget]::Machine
-            User = [EnvironmentVariableTarget]::User
+        $envRegKey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Environment', $true)
+        if ($Container -eq "Machine") {
+            $envRegKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\ControlSet001\Control\Session Manager\Environment\',$true)
         }
-        $containerType = $containerMapping[$Container]
 
-        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
-        if ($persistedPaths -notcontains $Path) {
-            $persistedPaths = $persistedPaths + $Path | Where-Object { $_ }
-            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        $envPath = $envRegKey.GetValue('Path', [string]::Empty, 'DoNotExpandEnvironmentNames').ToString()
+        $envPaths = $envPath -split [System.IO.Path]::PathSeparator
+        if ($envPaths -notcontains $Path) {
+            $envPaths = $envPaths + $Path | Where-Object { $_ }
+
+            # NEVER use [Environment]::SetEnvironmentVariable() for PATH values; see https://github.com/dotnet/corefx/issues/36449
+            # This issue exists in ALL released versions of .NET and .NET Core as of 12/19/2019
+            $envRegKey.SetValue('Path', $envPaths -join [System.IO.Path]::PathSeparator, 'ExpandString')
         }
     }
 
-    $envPaths = $env:Path -split ';'
+    $envPaths = $env:Path -split [System.IO.Path]::PathSeparator
     if ($envPaths -notcontains $Path) {
         $envPaths = $envPaths + $Path | Where-Object { $_ }
-        $env:Path = $envPaths -join ';'
+        $env:Path = $envPaths -join [System.IO.Path]::PathSeparator
     }
 }
 
