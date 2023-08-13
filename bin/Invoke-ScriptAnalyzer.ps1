4ef5b765dadcfd4aac9c21b706efdb7c86179f9c
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-08-13 18:11:14 +0200

    windows improvements

--- bin/Invoke-ScriptAnalyzer.ps1
+++ bin/Invoke-ScriptAnalyzer.ps1
@@ -3,11 +3,10 @@
 Set-StrictMode -Version 3.0
 $ErrorActionPreference = "Stop"
 
-if (-not (Get-InstalledModule -Name PSScriptAnalyzer -ErrorAction SilentlyContinue)) {
+if (-not (Get-Module -ListAvailable -Name PSScriptAnalyzer -ErrorAction SilentlyContinue)) {
     Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force
 }
 
-# Write-Host @args
 Import-Module PSScriptAnalyzer
 
 Invoke-ScriptAnalyzer @args
