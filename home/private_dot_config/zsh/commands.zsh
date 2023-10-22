e9491f0f896e13b7f97132c0830cc50b24948685
Jan-Willem Mulder <jwnmulder@gmail.com>
2023-10-22 22:04:46 +0200

    Added bat

--- home/private_dot_config/zsh/commands.zsh
+++ home/private_dot_config/zsh/commands.zsh
@@ -1,8 +1,8 @@
-if (( $+commands[batcat] )); then
+if (( $+commands[bat] )); then
+    alias cat='bat --paging=never --plain'
+elif (( $+commands[batcat] )); then
     alias cat='batcat --paging=never --plain'
     alias bat='batcat'
-elif (( $+commands[bat] )); then
-    alias cat='bat --paging=never --plain'
 fi
 
 alias wget='wget --hsts-file="$XDG_DATA_HOME"/wget-hsts'
