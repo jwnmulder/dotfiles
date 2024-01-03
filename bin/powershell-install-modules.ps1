3c646fd63926eb4b20601475c03deafcda325f4f
Jan-Willem Mulder <jwnmulder@gmail.com>
2024-01-03 22:23:26 +0100

    Improved powershell module setup

--- bin/powershell-install-modules.ps1
+++ bin/powershell-install-modules.ps1
@@ -6,16 +6,29 @@ Write-Output "PSVersion=${PSVersion}, PSModulePath=${env:PSModulePath}"
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
-if (-not (Get-Module -ListAvailable -FullyQualifiedName @{ModuleName="PackageManagement";ModuleVersion="1.4.8"})) {
-    Install-Module PackageManagement -Scope CurrentUser -MinimumVersion 1.4.8.0 -Force -AllowClobber
+function Install-Module-If-Missing {
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$Name,
+
+        [Parameter(Mandatory,Position=1)]
+        [string]$MinimumVersion,
+
+        [string]$Scope = "CurrentUser",
+        [switch]$Force,
+        [switch]$AllowClobber
+    )
+
+    if (-not (Get-Module -ListAvailable -FullyQualifiedName @{ModuleName=$Name;ModuleVersion=$MinimumVersion})) {
+        Install-Module -Name $Name -Scope $Scope -MinimumVersion $MinimumVersion -Force:$Force -AllowClobber:$AllowClobber -ErrorAction 'Stop'
+    }
 }
 
 # Needed for GitHubs 'windows-latest' image. Somehow version 1.4.7 is selected which is not working
+Install-Module-If-Missing -Name "PackageManagement" -MinimumVersion 1.4.8 -Force -AllowClobber
 Import-Module PackageManagement -MinimumVersion 1.4.8
 
-if (-not (Get-Module -ListAvailable -FullyQualifiedName @{ModuleName="PowerShellGet";ModuleVersion="2.0"})) {
-    Install-Module PowerShellGet -Scope CurrentUser -MinimumVersion 2.0 -Force -AllowClobber
-}
+Install-Module-If-Missing -Name "PowerShellGet" -MinimumVersion 2.0 -Force -AllowClobber
 
 # '-ForceBootstrap' will Install the NuGet package provider if not already done
 Get-PackageProvider -Name "NuGet" -ForceBootstrap
@@ -25,12 +38,11 @@ if (-not ((Get-PSRepository -Name "PSGallery" -ErrorAction SilentlyContinue).Ins
 }
 
 # PSScriptAnalyzer
-if (-not (Get-Module -ListAvailable -FullyQualifiedName @{ModuleName="PSScriptAnalyzer";ModuleVersion="1.0"})) {
-    Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
-}
+Install-Module-If-Missing -Name PSScriptAnalyzer -MinimumVersion 1.0
 
 # 2.1.0 has support for Predictive IntelliSense (F2 switches between InlineView and ListView)
 # https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/using-predictors?view=powershell-7.3$
-if (-not (Get-Module -ListAvailable -FullyQualifiedName @{ModuleName="PSReadLine";ModuleVersion="2.2.6"})) {
-    Install-Module PSReadLine -Scope CurrentUser -MinimumVersion 2.2.6 -Force -AllowClobber
-}
+Install-Module-If-Missing -Name PSReadLine -MinimumVersion 2.2.6 -Force -AllowClobber
+
+# WslInterop
+Install-Module-If-Missing -Name WslInterop -MinimumVersion 0.4.0
