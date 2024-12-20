e63fc45b7032fcc8668f6b89c91b158136b18e86
Jan-Willem Mulder <jwnmulder@gmail.com>
2024-12-20 22:05:34 +0100

    Handle non TTY environments a bit better

--- home/.chezmoihooks/linux/decrypt-private-age-key.sh
+++ home/.chezmoihooks/linux/decrypt-private-age-key.sh
@@ -3,9 +3,12 @@
 set -euo pipefail
 
 if [ ! -f "${HOME}/.config/chezmoi/key.txt" ]; then
-    mkdir -p "${HOME}/.config/chezmoi"
-
-    echo "Requesting decryption of age key. Please enter password"
-    "$CHEZMOI_EXECUTABLE" age decrypt --output "${HOME}/.config/chezmoi/key.txt" --passphrase "${CHEZMOI_WORKING_TREE}/home/.data/key.txt.age"
-    chmod 600 "${HOME}/.config/chezmoi/key.txt"
+    if [ -t 1 ]; then
+        mkdir -p "${HOME}/.config/chezmoi"
+        echo "Requesting decryption of age key. Please enter password"
+        "$CHEZMOI_EXECUTABLE" age decrypt --output "${HOME}/.config/chezmoi/key.txt" --passphrase "${CHEZMOI_WORKING_TREE}/home/.data/key.txt.age"
+        chmod 600 "${HOME}/.config/chezmoi/key.txt"
+    else
+        echo "WARNING: No TTY available to decrypt age key"
+    fi
 fi
