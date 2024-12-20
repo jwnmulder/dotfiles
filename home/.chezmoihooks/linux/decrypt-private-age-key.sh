46dee7f8f368f48e975a33a717ac787b360e9b79
Jan-Willem Mulder <jwnmulder@gmail.com>
2024-12-20 21:57:23 +0100

    gh actions debugging

--- home/.chezmoihooks/linux/decrypt-private-age-key.sh
+++ home/.chezmoihooks/linux/decrypt-private-age-key.sh
@@ -5,11 +5,7 @@ set -euo pipefail
 if [ ! -f "${HOME}/.config/chezmoi/key.txt" ]; then
     mkdir -p "${HOME}/.config/chezmoi"
 
-    env
-
-    ls -la /__w/dotfiles/dotfiles/home/
-
     echo "Requesting decryption of age key. Please enter password"
-    "$CHEZMOI_EXECUTABLE" age decrypt --output "${HOME}/.config/chezmoi/key.txt" --passphrase "${HOME}/.local/share/chezmoi/home/.data/key.txt.age"
+    "$CHEZMOI_EXECUTABLE" age decrypt --output "${HOME}/.config/chezmoi/key.txt" --passphrase "${CHEZMOI_WORKING_TREE}/home/.data/key.txt.age"
     chmod 600 "${HOME}/.config/chezmoi/key.txt"
 fi
