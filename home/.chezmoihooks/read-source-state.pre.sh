3ae929b6ca506861b32688cb72b59297263f6d2e
Jan-Willem Mulder <jwnmulder@gmail.com>
2024-12-22 15:54:36 +0100

    optional age key decryption

--- home/.chezmoihooks/read-source-state.pre.sh
+++ home/.chezmoihooks/read-source-state.pre.sh
@@ -2,9 +2,21 @@
 
 set -euo pipefail
 
-echo "hook: $*"
-
 HOOKS_BASE_DIR="$CHEZMOI_SOURCE_DIR/.chezmoihooks"
+
+action=""
+decrypt_key="false"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        read-source-state|init|update) action="$1" ;;
+        --decrypt_key) decrypt_key="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+echo "hook: action=$action, decrypt_key=$decrypt_key"
 
 # "$HOOKS_BASE_DIR"/linux/ensure-pre-requisites.sh
 "$HOOKS_BASE_DIR"/linux/decrypt-age-key.sh
