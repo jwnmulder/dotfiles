3ed0d0e0c3b8fc1460f3aa8514333de09e6542c9
Jan-Willem Mulder <jwnmulder@gmail.com>
2025-03-22 23:33:41 +0100

    ci - testing

--- bin/powershell-install-resources.ps1
+++ bin/powershell-install-resources.ps1
@@ -6,6 +6,20 @@ Write-Output "PSVersion=${PSVersion}, PSModulePath=${env:PSModulePath}"
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
+# https://learn.microsoft.com/en-US/powershell/gallery/powershellget/install-powershellget?view=powershellget-3.x
+# if (-not (Get-Command Install-PSResource -FullyQualifiedModule @{ModuleName="Microsoft.PowerShell.PSResourceGet";ModuleVersion="1.1.0"} -ErrorAction SilentlyContinue)) {
+    Write-Output "Microsoft.PowerShell.PSResourceGet not installed, will install now"
+
+    # Update PowerShellGet on Powershell v5 as it is too old to install Microsoft.PowerShell.PSResourceGet
+    if ($PSVersion.Major -eq 5) {
+        Write-Output "Install/update PowerShellGet"
+        Install-Module -Name "PowerShellGet" -Scope CurrentUser -Force -AllowClobber
+    }
+
+    Write-Output "Install/update Microsoft.PowerShell.PSResourceGet"
+    Install-Module -Name "Microsoft.PowerShell.PSResourceGet" -Scope CurrentUser -Repository PSGallery -Force
+# }
+
 # Trust PSGallery for PowerShellGet
 if (Get-Command Get-PSRepository -ErrorAction SilentlyContinue) {
     if (-not ((Get-PSRepository -Name "PSGallery" -ErrorAction SilentlyContinue).InstallationPolicy -eq "Trusted")) {
