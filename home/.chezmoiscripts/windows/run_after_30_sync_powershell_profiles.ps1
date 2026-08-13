fd395756bfcdd6d7ce231a3b8e2edc9790d81545
Jan-Willem Mulder <jan.willem.mulder@nn.be>
2026-08-13 11:42:26 +0200

    chore: ensure Powershell profiles directories exist (OneDrive)

--- home/.chezmoiscripts/windows/run_after_30_sync_powershell_profiles.ps1
+++ home/.chezmoiscripts/windows/run_after_30_sync_powershell_profiles.ps1
@@ -1,10 +1,19 @@
 Set-StrictMode -Version 3.0
 $ErrorActionPreference = "Stop"
 
-$userprofile_documents = "$env:USERPROFILE\Documents"
-$mydocuments = [environment]::getfolderpath("mydocuments")
+$userprofile_documents = "$env:USERPROFILE\Documents"  # local folder
+$mydocuments = [environment]::getfolderpath("mydocuments")  # can be a local or OneDrive folder
+
+# Prints all profile locations
+# $profile | Select-Object *
 
 if (-Not $mydocuments.ToLower().StartsWith($userprofile_documents.ToLower())) {
+
+    # Copy profile to WindowsPowerShell (built-in PowerShell)
+    New-Item -Path "$mydocuments\WindowsPowerShell" -ItemType Directory -Force | Out-Null
     Copy-Item "$userprofile_documents\WindowsPowerShell\profile.ps1" -Destination "$mydocuments\WindowsPowerShell\" -Recurse
+
+    # Copy profile to PowerShell (separately installed PowerShell, version 7+)
+    New-Item -Path "$mydocuments\PowerShell" -ItemType Directory -Force | Out-Null
     Copy-Item "$userprofile_documents\WindowsPowerShell\profile.ps1" -Destination "$mydocuments\PowerShell\" -Recurse
 }
